9191d891be1b14a67d7033c75c716bcc0b711252
unknown <tuanhai20062000@gmail.com>
2019-11-10 01:12:53 +0700

    a

--- 1.js
+++ 1.js
@@ -6,5 +6,9 @@
   $("#kn").click(function() {
  	$('body,html').animate({scrollTop:vitricuaedu-100},300)
  });
+  vitricuacmt=$('#cmt').offset().top;
+  $("#cm").click(function() {
+ 	$('body,html').animate({scrollTop:vitricuacmt-500},300)
+ });
 })  
  
