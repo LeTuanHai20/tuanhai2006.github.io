ae92df890a15d8cf876c614152b8afab7fd07909
unknown <tuanhai20062000@gmail.com>
2019-11-10 00:49:42 +0700

    ok

--- 1.js
+++ 1.js
@@ -1,4 +1,10 @@
 ﻿ $(function(){
- 
+ vitricuaedu=$('#khoiedu').offset().top;
+ $(".xuong").click(function() {
+ 	$('body,html').animate({scrollTop:vitricuaedu-100},300)
+ });
+  $("#kn").click(function() {
+ 	$('body,html').animate({scrollTop:vitricuaedu-100},300)
+ });
 })  
  
